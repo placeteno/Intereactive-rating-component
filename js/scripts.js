992da709b1eea5b8500716609414bb1f0928cd62
placeteno <joseraul88@gmail.com>
2022-07-07 12:08:55 -0500

    Project completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,18 @@
-let submitBtn = document.getElementById('submit-btn');
-let cardTwo = document.getElementById('card2');
-let ratingOne = document.getElementById('n1');
-let ratingTwo = document.getElementById('n2');
-let ratingThree = document.getElementById('n3');
-let ratingFour = document.getElementById('n4');
-let ratingFive = document.getElementById('n5');
-let ratingNbr = document.querySelector('span');
+const cardOne = document.querySelector('.card-one');
+const cardTwo = document.querySelector('.card-two');
+const rating = document.querySelectorAll('#rating li');
+const submitBtn = document.querySelector('.submit-btn a');
+const span = document.querySelector('span');
 
+rating.forEach(number => {
+  number.addEventListener('click', e => {
+    let dataNum = e.target.dataset.num;
+    rating.forEach(number => number.classList.remove('selected'));
+    number.classList.add('selected');
+    span.textContent = dataNum;
+  });
+});
 
-function updateRating(event) {
-  let rating = event.target.innerHTML;
-  ratingNbr.innerHTML = rating;
-  }
-
-function changeCards() {
-  cardTwo = cardTwo.style.zIndex = '1';
-}
-
-ratingOne.addEventListener('click', updateRating);
-ratingTwo.addEventListener('click', updateRating)
-ratingThree.addEventListener('click', updateRating)
-ratingFour.addEventListener('click', updateRating)
-ratingFive.addEventListener('click', updateRating)
-submitBtn.addEventListener('click', changeCards);
+submitBtn.addEventListener('click', function () {
+  cardTwo.classList.add('z-index');
+});
